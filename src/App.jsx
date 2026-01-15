49da638bce06cfb99a1c921f0de6e8248c7b50f0
Adarsh goswami <adarshgoswami221206@gmail.com>
2026-01-15 12:25:20 +0530

    css change

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,41 @@ function App() {
   return (
     <div className="w-full h-screen"
       style={{backgroundColor: color}}>
-        <div className=" flex fixed w-full  justify-center bottom-12 inset-x-0 p-4 text-white text-2xl font-bold rounded-lg">
-          <div className="flex justify-center bg-white text-black px-4 py-1 gap-7">
-            <button className=" h-full outline-0 "
+        <div className="bg-amber-100 w-full  flex fixed flex-wrap justify-center bottom-12 inset-x-0 px-2 text-white  font-bold rounded-lg">
+          <div className="flex w-screen flex-wrap h-10  justify-center bg-white  px-4 py-1 gap-9 rounded-lg shadow-lg">
+            <button className=" w-15  outline-0"
             onClick={() => setColor("Red")}
             style={{backgroundColor: "red"}}>Red</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className="w-15  outline-0 "
             onClick={() => setColor("green")}
             style={{backgroundColor: "green"}}>Green</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className=" w-15  outline-0 "
             onClick={() => setColor("blue")}
             style={{backgroundColor: "blue"}}>Blue</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className=" w-15  outline-0 "
             onClick={() => setColor("yellow")}
             style={{backgroundColor: "yellow"}}>Yellow</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className="w-15  outline-0 "
             onClick={() => setColor("purple")}
             style={{backgroundColor: "purple"}}>Purple</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className="w-15 outline-0 "
             onClick={() => setColor("olive")}
             style={{backgroundColor: "olive"}}>Olive</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className="w-15  outline-0 "
             onClick={() => setColor("skyblue")}
             style={{backgroundColor: "skyblue"}}>Skyblue</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className="w-15  outline-0"
             onClick={() => setColor("teal")}
             style={{backgroundColor: "teal"}}>Teal</button>
 
-            <button className="w-full h-full outline-0 "
+            <button className="w-15  outline-0 "
             onClick={() => setColor("orange")}
             style={{backgroundColor: "orange"}}>Orange</button> 
 
